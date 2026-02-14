139575ecb68e032b9e946463574301c86d17030d
OmarMahmou22 <omahmoudali9@gmail.com>
2026-02-14 11:05:34 +0200

    v2

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+
+    // cart: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Cart'
+    // }
+   
+} , {timestamps:true})
+
+const user = mongoose.model('User', UserSchema)
+
+module.exports = user
